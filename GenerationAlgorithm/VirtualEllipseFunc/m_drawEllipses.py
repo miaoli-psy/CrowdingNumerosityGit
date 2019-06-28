254c1488a09dcb3abe0fb63446d59b67c70e6ef3
ljm250 <jiamianliu@outlook.com>
2019-06-28 22:37:57 +0200

    modify save multi pics

--- GenerationAlgorithm/VirtualEllipseFunc/m_drawEllipses.py
+++ GenerationAlgorithm/VirtualEllipseFunc/m_drawEllipses.py
@@ -38,14 +38,14 @@ def drawEllipse (e_posi, ka, kb, crowding_cons, newWindowSize, loop_number):
         e.set_facecolor(np.random.rand(3))
     ax.set_xlim([-800, 800])
     ax.set_ylim([-500, 500])
-    ax.set_title('c_%s_wS_%s_eS_%s_%s_E.png' %(crowding_cons,newWindowSize,ka,kb))
+    ax.set_title('c_%s_wS_%s_eS_%s_%s_E_%s.png' %(crowding_cons,newWindowSize,ka,kb,len(e_posi)))
     try:
         loop_number
     except NameError:
         var_exists = False
     else:
         var_exists = True
-        plt.savefig('%s_c_%s_wS_%s_eS_%s_%s_E.png' %(loop_number,crowding_cons,newWindowSize,ka,kb))
+        plt.savefig('%s_c_%s_wS_%s_eS_%s_%s_E_%s.png' %(loop_number,crowding_cons,newWindowSize,ka,kb,len(e_posi)))
 
 # call the function
 #eposi = [(-90.0, -60.0), (140.0, 110.0), (-110.0, 150.0), (-140.0, -160.0), (130.0, -20.0), (200.0, 10.0), (200.0, -100.0), (120.0, 180.0), (-230.0, -50.0), (-200.0, 50.0), (-40.0, -140.0), (-220.0, 170.0), (-190.0, 0.0), (140.0, -130.0), (10.0, 120.0), (-100.0, 90.0), (60.0, 160.0), (30.0, 100.0), (-110.0, -10.0), (70.0, -160.0), (220.0, -50.0), (-160.0, -80.0), (250.0, -180.0), (-60.0, 170.0), (70.0, -80.0), (210.0, 110.0), (0.0, -120.0), (-90.0, -170.0), (-20.0, 130.0), (130.0, 50.0), (-160.0, 90.0), (100.0, -60.0), (-70.0, -80.0), (120.0, 10.0), (100.0, -140.0), (-240.0, -180.0), (50.0, 90.0), (80.0, 70.0), (30.0, -140.0), (-100.0, 40.0), (-110.0, -40.0), (-40.0, 100.0), (220.0, 60.0), (-50.0, -90.0), (-120.0, 20.0), (-60.0, 80.0), (50.0, -90.0)]
