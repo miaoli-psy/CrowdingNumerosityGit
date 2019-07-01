e9997ab2655737611888ddb8cfa19e5b15c56ede
MiaoLi <chuoli223@yahoo.com>
2019-07-01 21:52:14 +0200

    modify m_drawEllipses.py, add drawing processes

--- GenerationAlgorithm/VirtualEllipseFunc/m_drawEllipses.py
+++ GenerationAlgorithm/VirtualEllipseFunc/m_drawEllipses.py
@@ -34,11 +34,23 @@ def drawEllipse (e_posi, ka, kb, crowding_cons, newWindowSize, loop_number):
     for e in my_e:
         ax.add_artist(e)
         e.set_clip_box(ax.bbox)
-        e.set_alpha(np.random.rand())
-        e.set_facecolor(np.random.rand(3))
-    ax.set_xlim([-800, 800])
-    ax.set_ylim([-500, 500])
-    ax.set_title('c_%s_wS_%s_eS_%s_%s_E_%s.png' %(crowding_cons,newWindowSize,ka,kb,len(e_posi)))
+        # e.set_alpha(np.random.rand())
+        # e.set_facecolor(np.random.rand(3))
+    # ax.set_xlim([-800, 800])
+    # ax.set_ylim([-500, 500])
+    ax.set_xlim([-400, 400])
+    ax.set_ylim([-250, 250])
+    # ax.set_title('c_%s_wS_%s_eS_%s_%s_E_%s.png' %(crowding_cons,newWindowSize,ka,kb,len(e_posi)))
+    
+    #边框不可见
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    #坐标不可见
+    ax.axes.get_yaxis().set_visible(False)
+    ax.axes.get_xaxis().set_visible(False)
+    
     try:
         loop_number
     except NameError:
@@ -48,12 +60,27 @@ def drawEllipse (e_posi, ka, kb, crowding_cons, newWindowSize, loop_number):
         plt.savefig('%s_c_%s_wS_%s_eS_%s_%s_E_%s.png' %(loop_number,crowding_cons,newWindowSize,ka,kb,len(e_posi)))
 
 # call the function
-#eposi = [(-90.0, -60.0), (140.0, 110.0), (-110.0, 150.0), (-140.0, -160.0), (130.0, -20.0), (200.0, 10.0), (200.0, -100.0), (120.0, 180.0), (-230.0, -50.0), (-200.0, 50.0), (-40.0, -140.0), (-220.0, 170.0), (-190.0, 0.0), (140.0, -130.0), (10.0, 120.0), (-100.0, 90.0), (60.0, 160.0), (30.0, 100.0), (-110.0, -10.0), (70.0, -160.0), (220.0, -50.0), (-160.0, -80.0), (250.0, -180.0), (-60.0, 170.0), (70.0, -80.0), (210.0, 110.0), (0.0, -120.0), (-90.0, -170.0), (-20.0, 130.0), (130.0, 50.0), (-160.0, 90.0), (100.0, -60.0), (-70.0, -80.0), (120.0, 10.0), (100.0, -140.0), (-240.0, -180.0), (50.0, 90.0), (80.0, 70.0), (30.0, -140.0), (-100.0, 40.0), (-110.0, -40.0), (-40.0, 100.0), (220.0, 60.0), (-50.0, -90.0), (-120.0, 20.0), (-60.0, 80.0), (50.0, -90.0)]
+eposi = [(-90.0, -60.0), (140.0, 110.0), (-110.0, 150.0), (-140.0, -160.0), (130.0, -20.0), (200.0, 10.0), (200.0, -100.0), (120.0, 180.0), (-230.0, -50.0), (-200.0, 50.0), (-40.0, -140.0), (-220.0, 170.0), (-190.0, 0.0), (140.0, -130.0), (10.0, 120.0), (-100.0, 90.0), (60.0, 160.0), (30.0, 100.0), (-110.0, -10.0), (70.0, -160.0), (220.0, -50.0), (-160.0, -80.0), (250.0, -180.0), (-60.0, 170.0), (70.0, -80.0), (210.0, 110.0), (0.0, -120.0), (-90.0, -170.0), (-20.0, 130.0), (130.0, 50.0), (-160.0, 90.0), (100.0, -60.0), (-70.0, -80.0), (120.0, 10.0), (100.0, -140.0), (-240.0, -180.0), (50.0, 90.0), (80.0, 70.0), (30.0, -140.0), (-100.0, 40.0), (-110.0, -40.0), (-40.0, 100.0), (220.0, 60.0), (-50.0, -90.0), (-120.0, 20.0), (-60.0, 80.0), (50.0, -90.0)]
 #drawEllipse(e_posi = eposi, ka = 0.25, kb = 0.1, crowding_cons = 0, newWindowSize= 0.5, loop_number=1)
+for posi in eposi:
+    plt.plot(posi[0],posi[1],'ko')
+ex = plt.gca()
+ex.set_xlim([-400, 400])
+ex.set_ylim([-250, 250])
 
+#边框不可见
+ex.spines['top'].set_visible(False)
+ex.spines['right'].set_visible(False)
+ex.spines['bottom'].set_visible(False)
+ex.spines['left'].set_visible(False)
+#坐标不可见
+ex.axes.get_yaxis().set_visible(False)
+ex.axes.get_xaxis().set_visible(False)
 
+# plt.show()
+plt.savefig('dots.png')
 # call the function
-eposi = [(-90.0, -60.0), (140.0, 110.0), (-110.0, 150.0), (-140.0, -160.0)]
+# eposi = [(-90.0, -60.0), (140.0, 110.0), (-110.0, 150.0), (-140.0, -160.0)]
 
 def drawProcess(e_posi, ka, kb, crowding_cons, newWindowSize, loop_number):
     curr_eposi = []
