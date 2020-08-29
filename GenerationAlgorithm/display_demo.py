26a291f8f6390b93c639eb67f2a59c3510d33eb4
miaoli_psy <chuoli223@yahoo.com>
2020-08-30 00:54:41 +0200

    include both exp1 and exp2 demo

--- GenerationAlgorithm/display_demo.py
+++ GenerationAlgorithm/display_demo.py
@@ -45,6 +45,10 @@ baseline = [(-120.0, -20.0), (-270.0, 30.0), (-40.0, -180.0), (-10.0, 180.0), (-
 exp1_c  = [(20.0, -170.0), (230.0, -100.0), (-60.0, -140.0), (300.0, -20.0), (-110.0, 160.0), (60.0, 140.0), (230.0, 90.0), (100.0, 70.0), (120.0, -210.0), (-20.0, 210.0), (-200.0, 100.0), (-70.0, 80.0), (-250.0, -160.0), (100.0, -80.0), (-260.0, 30.0), (-260.0, 180.0), (210.0, -10.0), (-140.0, -160.0), (300.0, 160.0), (-180.0, -10.0), (-80.0, 130.0), (170.0, -100.0), (180.0, 90.0), (-140.0, 80.0), (-20.0, -110.0), (50.0, -100.0), (-100.0, -60.0), (-110.0, 60.0), (250.0, -180.0), (140.0, 20.0), (140.0, 180.0), (-40.0, -200.0), (150.0, -50.0), (-140.0, 0.0), (-10.0, 130.0), (-130.0, -70.0), (-150.0, 210.0), (130.0, 100.0), (40.0, 110.0), (80.0, -60.0), (200.0, 210.0), (100.0, -10.0), (-140.0, -220.0), (-100.0, -10.0), (90.0, -130.0), (-60.0, -80.0), (-10.0, 100.0), (-220.0, -100.0), (80.0, 170.0), (-10.0, 170.0), (20.0, -130.0), (-290.0, -220.0), (100.0, -170.0)]
 exp1_nc = [(50.0, 150.0), (-170.0, 140.0), (0.0, -150.0), (-190.0, 10.0), (-180.0, 80.0), (200.0, -50.0), (100.0, -140.0), (290.0, 80.0), (220.0, 150.0), (150.0, -70.0), (80.0, -210.0), (-210.0, -60.0), (-120.0, 30.0), (280.0, -180.0), (-120.0, 170.0), (-260.0, -210.0), (140.0, 190.0), (-110.0, -70.0), (130.0, 10.0), (-50.0, -210.0), (150.0, 80.0), (0.0, 100.0), (-60.0, -110.0), (-280.0, 60.0), (-300.0, 210.0), (-60.0, 190.0), (-90.0, 80.0), (-120.0, -30.0), (-130.0, -130.0), (10.0, 180.0), (40.0, -110.0), (210.0, -220.0), (300.0, -30.0), (-60.0, 120.0), (-140.0, -200.0), (130.0, 40.0), (110.0, -80.0), (-20.0, -100.0), (-300.0, -30.0), (90.0, 60.0), (-200.0, -110.0), (90.0, 110.0), (-70.0, -80.0), (30.0, -220.0), (110.0, -30.0), (70.0, -80.0), (-30.0, 110.0), (230.0, 20.0), (300.0, -110.0), (-100.0, 50.0), (-60.0, 80.0), (-100.0, 0.0), (50.0, 90.0)]
 
+exp1_crowding =[(20.0, -170.0), (230.0, -100.0), (-60.0, -140.0), (300.0, -20.0), (-110.0, 160.0), (60.0, 140.0), (230.0, 90.0), (100.0, 70.0), (120.0, -210.0), (-20.0, 210.0), (-200.0, 100.0), (-70.0, 80.0), (-250.0, -160.0), (100.0, -80.0), (-260.0, 30.0), (-260.0, 180.0), (210.0, -10.0), (-140.0, -160.0), (300.0, 160.0), (-180.0, -10.0), (-80.0, 130.0), (170.0, -100.0), (180.0, 90.0), (-140.0, 80.0), (-20.0, -110.0), (50.0, -100.0), (-100.0, -60.0), (-110.0, 60.0), (250.0, -180.0), (140.0, 20.0), (140.0, 180.0), (-40.0, -200.0), (150.0, -50.0), (-140.0, 0.0), (-10.0, 130.0), (-130.0, -70.0), (-150.0, 210.0), (130.0, 100.0), (40.0, 110.0), (80.0, -60.0), (200.0, 210.0), (100.0, -10.0), (-140.0, -220.0), (-100.0, -10.0), (90.0, -130.0), (-60.0, -80.0), (-10.0, 100.0), (-220.0, -100.0), (80.0, 170.0), (-10.0, 170.0), (20.0, -130.0), (-290.0, -220.0), (100.0, -170.0)]
+
+exp1_no_crowding = [(-240.0, -190.0), (-90.0, -130.0), (-270.0, -90.0), (-50.0, -210.0), (210.0, 170.0), (-300.0, 190.0), (300.0, -190.0), (-40.0, 160.0), (60.0, -140.0), (120.0, 50.0), (-260.0, 10.0), (250.0, -30.0), (300.0, 130.0), (140.0, -80.0), (30.0, 190.0), (100.0, 170.0), (190.0, 30.0), (120.0, -120.0), (-180.0, 80.0), (20.0, -190.0), (-40.0, -100.0), (-110.0, -50.0), (180.0, -210.0), (260.0, -90.0), (-270.0, 70.0), (70.0, 100.0), (-190.0, 170.0), (120.0, 120.0), (-80.0, 70.0), (80.0, -60.0), (-110.0, -80.0), (-180.0, -20.0), (150.0, -10.0), (-10.0, 120.0), (30.0, 100.0), (-180.0, -220.0), (-60.0, 100.0), (-130.0, 180.0), (-10.0, 220.0), (-140.0, 10.0), (20.0, -100.0), (-110.0, -20.0), (130.0, -40.0), (-10.0, -130.0), (-90.0, 50.0), (60.0, -90.0), (-100.0, 30.0), (-110.0, -220.0), (120.0, -220.0), (100.0, 20.0), (-90.0, 210.0), (-180.0, -100.0), (100.0, 70.0)]
+# =============================================================================
 # draw ellipse
 # =============================================================================
 def drawEllipse_full(e_posi, extra_posi, ka, kb,ellipseColor_r = 'orangered', ellipseColor_t = 'lime'):
@@ -93,6 +97,7 @@ def drawEllipse_full(e_posi, extra_posi, ka, kb,ellipseColor_r = 'orangered', el
         # plt.show()
         for dot1 in extra_posi:
             plt.plot(dot1[0],dot1[1],color = 'r', marker = 'o', markersize = 2)
+        plt.plot(0,0,color = 'k', marker = '+', markersize = 4)
         # plt.show()
         # ax.set_xlim([-800, 800])
         # ax.set_ylim([-500, 500])
@@ -112,7 +117,7 @@ def drawEllipse_full(e_posi, extra_posi, ka, kb,ellipseColor_r = 'orangered', el
         
         plt.savefig('try.png',bbox_inches = 'tight', pad_inches = 0)
 
-def drawEllipses (posi, ka, kb, crowding_cons = 1, ellipseColor = 'orangered', ellipsetransp = 0.5):
+def drawEllipses(posi, ka, kb, ellipseColor = 'orangered', ellipsetransp = 0.5):
 
     eccentricities2 = []
     for i in range(len(posi)):
@@ -166,10 +171,107 @@ def drawEllipses (posi, ka, kb, crowding_cons = 1, ellipseColor = 'orangered', e
     
     plt.savefig('try.png',bbox_inches = 'tight', pad_inches = 0)
 
+def drawEllipse_crowding(e_posi, crowding_posi, ka, kb,ellipseColor_r = 'orangered', ellipseColor_t = 'lime'):
+        """
+        crowding display: show discs that falls into others crowding zones.
 
+        """
+        eccentricities = []
+        for i in range(len(e_posi)):
+            eccentricities0 = distance.euclidean(e_posi[i], (0,0))
+            eccentricities.append(eccentricities0)
+        #radial
+        angle_deg = []
+        for ang in range(len(e_posi)):
+            angle_rad0 = atan2(e_posi[ang][1],e_posi[ang][0])
+            angle_deg0 = angle_rad0*180/pi
+            angle_deg.append(angle_deg0)
+            
+        #crowding disc
+        eccentricities_c = []
+        for i in range(len(crowding_posi)):
+            eccentricities0 = distance.euclidean(crowding_posi[i], (0,0))
+            eccentricities_c.append(eccentricities0)
+        angle_deg_c = []
+        for ang in range(len(crowding_posi)):
+            angle_rad0 = atan2(crowding_posi[ang][1],crowding_posi[ang][0])
+            angle_deg0 = angle_rad0*180/pi
+            angle_deg_c.append(angle_deg0)
+        my_e = [Ellipse(xy=crowding_posi[j], width=eccentricities_c[j]*ka*2, height=eccentricities_c[j]*kb*2, angle = angle_deg_c[j])
+                for j in range(len(crowding_posi))]
+        
+        #tangential
+        angle_deg2 = []
+        for ang in range(len(e_posi)):
+            angle_rad0_2 = atan2(e_posi[ang][1],e_posi[ang][0])
+            angle_deg0_2 = angle_rad0_2*180/pi + 90
+            angle_deg2.append(angle_deg0_2)
+        my_e2 = [Ellipse(xy=e_posi[j], width=eccentricities[j]*ka*2, height=eccentricities[j]*kb*2, angle = angle_deg[j]+90)
+                for j in range(len(e_posi))]
+        
+        fig, ax = plt.subplots(subplot_kw={'aspect': 'equal'})
+        for e in my_e:
+            ax.add_artist(e)
+            e.set_clip_box(ax.bbox)
+            e.set_alpha(0.5)
+            e.set_facecolor(ellipseColor_r)
+        for e2 in my_e2:
+            ax.add_artist(e2)
+            e2.set_clip_box(ax.bbox)
+            e2.set_alpha(0.5)
+            e2.set_facecolor(ellipseColor_t)
+        
+        #show the discs on the ellipses-flower
+        for dot in e_posi:
+            plt.plot(dot[0],dot[1], color = 'k', marker ='o', markersize = 2 )
+        # plt.show()
+        plt.plot(0,0,color = 'k', marker = '+', markersize = 4)
+        # plt.show()
+        # ax.set_xlim([-800, 800])
+        # ax.set_ylim([-500, 500])
+        ax.set_xlim([-400, 400])
+        ax.set_ylim([-260, 260])
+        # ax.set_title('wS_%s_eS_%s_%s_E.png' %(newWindowSize,ka,kb))
+        
+        #边框不可见
+        ax.spines['top'].set_visible(False)
+        ax.spines['right'].set_visible(False)
+        ax.spines['bottom'].set_visible(False)
+        ax.spines['left'].set_visible(False)
+        #坐标不可见
+        ax.axes.get_yaxis().set_visible(False)
+        ax.axes.get_xaxis().set_visible(False)
+        ax.patch.set_facecolor('lightgray')
+        
+        plt.savefig('try.png',bbox_inches = 'tight', pad_inches = 0)
 
+def draw_disc_only(e_posi):
+    
+    fig, ax = plt.subplots(subplot_kw={'aspect': 'equal'})
+    for dot in e_posi:
+        plt.plot(dot[0],dot[1], color = 'k', marker ='o', markersize = 2)
+    plt.plot(0,0,color = 'k', marker = '+', markersize = 4)
+    
+    #set x,y lim
+    ax.set_xlim([-400, 400])
+    ax.set_ylim([-260, 260])
+    #边框不可见
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+    ax.spines['left'].set_visible(False)
+    #坐标不可见
+    ax.axes.get_yaxis().set_visible(False)
+    ax.axes.get_xaxis().set_visible(False)
+    # set background color
+    ax.patch.set_facecolor('lightgray')
+    
+    plt.savefig('try.png',bbox_inches = 'tight', pad_inches = 0)
+# =============================================================================
+# draw sample displays
+# =============================================================================
 #drawEllipse_full(centerposi, [], 0.25, 0.1)
-drawEllipse_full(centerposi, extra_c, 0.25, 0.1)
+# drawEllipse_full(centerposi, extra_c, 0.25, 0.1)
 #drawEllipse_full(centerposi, extra_nc, 0.25, 0.1)
 #drawEllipse_full(centerposi, extra_c_50p, 0.25, 0.1)
 #drawEllipse_full(centerposi, extra_nc_50p, 0.25, 0.1)
@@ -177,5 +279,20 @@ drawEllipse_full(centerposi, extra_c, 0.25, 0.1)
 #drawEllipse_full(centerposi, extra_nc_0p, 0.25, 0.1)
 #drawEllipse_full(baseline, [], 0.15,0.15)
 #drawEllipses(exp1_c, 0.1, 0.25,  ellipseColor = '2')
-
 #drawEllipse_full(extra_nc,centerposi , 0.25, 0.1)
+    
+drawEllipses(posi = exp1_crowding, ka=0.1, kb=0.25, ellipseColor = 'lime', ellipsetransp = 0.5)
+
+
+
+#exp 1 display sample
+draw_disc_only(exp1_crowding)
+#exp 1 display with ellipses no-crowding
+drawEllipses(posi = exp1_no_crowding, ka=0.25, kb=0.1, ellipseColor = 'orangered', ellipsetransp = 0.5)
+#exp1 display with ellipses crowding
+
+crowded_posi = [exp1_crowding[0],exp1_crowding[5],exp1_crowding[8],exp1_crowding[9],exp1_crowding[13],
+                exp1_crowding[23],exp1_crowding[34],exp1_crowding[36],exp1_crowding[46],exp1_crowding[48], 
+                exp1_crowding[49], exp1_crowding[51],exp1_crowding[52]]
+
+drawEllipse_crowding(exp1_crowding,crowded_posi, ka=0.25, kb=0.1, ellipseColor_r = 'orangered', ellipseColor_t = 'lime')
